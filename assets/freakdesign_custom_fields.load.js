2e4b2a5bf9211cb084fe83b1cfe667dc2ebd500c
iain <9ijn8uhb>
2015-05-14 09:28:10 -0400

    'first commit'

---
+++ assets/freakdesign_custom_fields.load.js
@@ -0,0 +1,32 @@
+/*
+
+
+      ############# 
+    ##            *## 
+   #        %      **#
+  #        %%%    ***#
+ #       %%F%D%%   ****#
+#          %%%    *****#
+#   ###     %     ###***# 
+#  # ####       #### #**# 
+#  #     #     #     #**# 
+#   #####  # #  #####***# 
+#         #   #  *******# 
+ ### #           **# ### 
+     # - - - - - - #                 
+      | | | | | | |
+	
+     freakdesign.com               
+
+CUSTOM FIELDS
+
+Written by jason from freakdesign
+freakdesign.com.au
+shopify.freakdesign.com.au
+Don't be a tool and pass this off as your own. Contribute!
+
+*/
+var custom_js=document.createElement("script");
+custom_js.type="text/javascript";
+custom_js.src="//rawgithub.com/freakdesign/shopify-custom-fields/master/freakdesign_custom_fields.min.js";
+document.getElementsByTagName('head')[0].appendChild(custom_js)
---
+++ custom/config.rb
@@ -0,0 +1,5 @@
+http_path = "/"
+css_dir = '../assets'
+sass_dir = '../custom'
+images_dir = '../images'
+fonts_dir = '../custom'
